ac51245b2f75fabb800fa277d3bd8ef7a58800fb
caspg <kacper.golinski@gmail.com>
2015-02-03 21:44:03 +0100

    validate ownership in product_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
+  before_action :authenticate_owner, only: [:edit, :update]
+
   expose(:category)
   expose(:products)
   expose(:product)
@@ -46,5 +49,13 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  # Test if current user is product owner. If not redirect to product.
+  def authenticate_owner 
+    if current_user != product.user
+      flash[:error] = 'You are not allowed to edit this product.'
+      redirect_to category_product_url(category, product)
+    end
   end
 end
