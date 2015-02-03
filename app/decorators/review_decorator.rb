ad9181ff542ffb589c0383fbd4b6aa82ba87a4f5
caspg <kacper.golinski@gmail.com>
2015-02-03 22:00:48 +0100

    add action to review_decorator

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,7 +1,12 @@
 class ReviewDecorator < Draper::Decorator
   delegate_all
 
-  def author  
+  # Return string with author's fullname.
+  def author
+    if user.firstname && user.lastname  
+      user.firstname + ' ' + user.lastname
+    else 
+      'not given'
+    end
   end
-
 end
