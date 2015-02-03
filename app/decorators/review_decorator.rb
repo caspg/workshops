41811db567105b45afefb8a0954f2cacf54115c5
caspg <kacper.golinski@gmail.com>
2015-02-04 00:29:21 +0100

    fix review_decorator

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -2,11 +2,7 @@ class ReviewDecorator < Draper::Decorator
   delegate_all
 
   # Return string with author's fullname.
-  def author
-    if user.firstname && user.lastname  
-      user.firstname + ' ' + user.lastname
-    else 
-      'not given'
-    end
+  def author 
+    user.firstname + ' ' + user.lastname
   end
 end
