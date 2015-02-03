0e1026e0992ceb7b4fcb19f53b6cab8358d386d0
caspg <kacper.golinski@gmail.com>
2015-02-03 18:36:46 +0100

    add product validation and average_rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  validates :description, presence: true
+  validates :price, presence: true, numericality: true, 
+                    format: { with: /\A\d+\.?\d{0,2}\z/, message: "price is limited to two decimal places" }
+  validates :title, presence: true
+
+  # Return calculated average rating or 0.0 if no ratings.
+  def average_rating
+    reviews.average("rating").to_f.round(1)
+  end
 end
