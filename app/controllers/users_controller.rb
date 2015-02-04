671b87643afa9817f5c275ccac4c15b9981c49e9
caspg <kacper.golinski@gmail.com>
2015-02-04 17:22:03 +0100

    list 5 last reviews on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
 
   def show
      @user = User.find(params[:id])
+     @reviews = @user.reviews.order('created_at desc').limit(5)
    end 
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:dmy] = '%d-%m-%Y'
