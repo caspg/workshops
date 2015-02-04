d93ac713d4dea4c8f5b5e1c4b031a43bc0a79291
caspg <kacper.golinski@gmail.com>
2015-02-04 11:35:06 +0100

    add current_user_admin? helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,8 @@
 module CategoriesHelper
+  # Return true if current_user exist and is admin, false otherwise.
+  def current_user_admin?
+    if user_signed_in?
+      current_user.admin?
+    end
+  end
 end
