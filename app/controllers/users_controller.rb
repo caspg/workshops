0fe4bb2e32270098f87b5500ec33e2ce00b374ed
caspg <kacper.golinski@gmail.com>
2015-02-04 23:01:20 +0100

    css and delete unnecessary files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  expose_decorated(:reviews, ancestor: :product)
-
+  
   def show
      @user = User.find(params[:id])
      @reviews = @user.reviews.order('created_at desc').limit(5)
