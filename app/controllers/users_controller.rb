3018a8f8503fde1f3d2f8d90a1d0578205b256ae
caspg <kacper.golinski@gmail.com>
2015-02-05 17:56:23 +0100

    add decent exposure to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  expose(:user)
+  expose(:reviews) { user.reviews.order('created_at desc').limit(5) }
   
   def show
-     @user = User.find(params[:id])
-     @reviews = @user.reviews.order('created_at desc').limit(5)
-   end 
+  end 
 end
