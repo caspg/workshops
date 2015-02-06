81cb1b0f9cbad9945d9fbec11f11517ddde038ac
caspg <kacper.golinski@gmail.com>
2015-02-06 10:33:12 +0100

    fill seeds.rb with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+
+# Admin user
+User.create!(firstname: 'Netguru',
+             lastname: 'Workshops',
+             email: 'netguru@workshops.com',
+             password: 'adminadmin',
+             password_confirmation: 'adminadmin',
+             admin: true)
+
+# Regular users
+5.times do
+  firstname = Faker::Name.first_name
+  lastname = Faker::Name.last_name
+  email = Faker::Internet.free_email
+  password = 'password'
+  User.create!(firstname: firstname,
+               lastname: lastname,
+               email: email,
+               password: password,
+               created_at: rand(0..10).days.ago)
+end
+
+# Categories
+Category.create!(name: 'Herb and Spice')
+
+# Products
+6.times do 
+  title = Faker::Food.herb_or_spice
+  description = Faker::Lorem.sentence
+  price = "#{rand(1..200)}.#{rand(1..99)}"
+  user_id = rand(2..6)
+  category_id = 1
+  Product.create!(title: title,
+                  description: description,
+                  price: price,
+                  user_id: user_id,
+                  category_id: category_id)
+end
+
+# Reviews
+(1..6).each do |product_id|
+  (2..rand(3..6)).each do |user_id|
+    content = Faker::Lorem.paragraph(rand(1..3))
+    rating = rand(1..5)
+    Review.create!(content: content,
+                   rating: rating,
+                   product_id: product_id,
+                   user_id: user_id)
+  end
+end
+
+
+
+
+
